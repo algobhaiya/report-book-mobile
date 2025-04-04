12df1130dbfd4273b34fc1a5925ee6a549458fed
algoBhaiya <abusayedahmadruet13@gmail.com>
2025-04-04 07:03:17 +0600

    initial commit

---
+++ src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.MobileApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace algoBhaiya.ReportBook.MobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.MobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace algoBhaiya.ReportBook.MobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
