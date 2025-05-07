78338568da07b986d52ddf853295b6f21c263cd1
algoBhaiya <algobhaiya@gmail.com>
2025-05-07 06:45:02 +0600

    #21 Add Settings page

---
+++ src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.Presentation/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Input;
+
+namespace algoBhaiya.ReportBook.Presentation.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        public List<int> ModificationDurationOptions { get; } = new() { 7, 15, 20, 30 };
+        public List<int> DataRemovalPeriodOptions { get; } = new() { 3, 6, 12, 18, 24 };
+
+        private int _selectedModificationDuration;
+        public int SelectedModificationDuration
+        {
+            get => _selectedModificationDuration;
+            set => SetProperty(ref _selectedModificationDuration, value);
+        }
+
+        private int _selectedDataRemovalPeriod;
+        public int SelectedDataRemovalPeriod
+        {
+            get => _selectedDataRemovalPeriod;
+            set => SetProperty(ref _selectedDataRemovalPeriod, value);
+        }
+
+        public ICommand SaveSettingsCommand { get; }
+
+        public SettingsViewModel()
+        {
+            // Load from Preferences or a config service
+            SelectedModificationDuration = Preferences.Get("ModificationDuration", 15);
+            SelectedDataRemovalPeriod = Preferences.Get("DataRemovalPeriod", 6);
+
+            SaveSettingsCommand = new Command(SaveSettings);
+        }
+
+        private async void SaveSettings()
+        {
+            Preferences.Set("ModificationDuration", SelectedModificationDuration);
+            Preferences.Set("DataRemovalPeriod", SelectedDataRemovalPeriod);
+
+            await Shell.Current.DisplayAlert("Success", "Settings updated!", "OK");
+
+            await Shell.Current.Navigation.PopAsync();
+        }
+    }
+
+}
