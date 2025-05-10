ca21b1348446487210591f4ca87fddad915b602f
algoBhaiya <algobhaiya@gmail.com>
2025-05-10 09:20:45 +0600

    #8 delete user flow

--- src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.Core/Entities/AppUser.cs
+++ src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.Core/Entities/AppUser.cs
@@ -10,6 +10,7 @@ namespace algoBhaiya.ReportBook.Core.Entities
 
         public string UserName { get; set; }
         public string PasswordHash { get; set; } // Simplified login for local
+        public bool IsDeleted { get; set; } = false;
     }
 
 }
--- src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.Presentation/Views/LoginPage.xaml.cs
+++ src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.Presentation/Views/LoginPage.xaml.cs
@@ -13,6 +13,7 @@ public partial class LoginPage : ContentPage
 
     public ObservableCollection<AppUser> ExistingUsers { get; set; } = new ();
     public Command<AppUser> UserTappedCommand { get; }
+    public Command<AppUser> RemoveUserCommand { get; }
 
     public LoginPage(
         IRepository<AppUser> repository,
@@ -37,14 +38,17 @@ public partial class LoginPage : ContentPage
                 _appNavigator.NavigateToMainShell();
             }
         });
+
+        RemoveUserCommand = new Command<AppUser>(async (user) => await OnRemoveUserClicked(user));
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
 
-        var users = await _repository.GetAllAsync();
         ExistingUsers.Clear();
+
+        var users = await _repository.GetListAsync(u => !u.IsDeleted);        
         foreach (var user in users)
             ExistingUsers.Add(user);
     }
@@ -76,10 +80,90 @@ public partial class LoginPage : ContentPage
             await _repository.AddAsync(user);
         }
 
+        if (user.IsDeleted)
+        {
+            user.IsDeleted = false;
+            await _repository.UpdateAsync(user);
+        }
+
         Preferences.Set("CurrentUserId", user.Id);
 
         await Navigation.PopAsync();
 
         _appNavigator.NavigateToMainShell(); // ?? Login done, show shell
-    }    
+    }
+
+    private async Task OnRemoveUserClicked(AppUser user)
+    {
+        string action = await DisplayActionSheet(
+            $"Remove user '{user.UserName}'?",
+            "Cancel",
+            null,
+            "Hide User (Soft Delete)",
+            "Delete Permanently");
+
+        switch (action)
+        {
+            case "Hide User (Soft Delete)":
+                user.IsDeleted = true; // You need to add this flag in your AppUser entity
+                await _repository.UpdateAsync(user);
+                break;
+
+            case "Delete Permanently":
+                await DeleteUserPermanentlyAsync(user);
+                break;
+        }
+
+        // Refresh the user list        
+        ExistingUsers.Clear();
+
+        var users = await _repository.GetListAsync(u => !u.IsDeleted);
+        foreach (var u in users)
+            ExistingUsers.Add(u);
+    }
+
+    private async Task DeleteUserPermanentlyAsync(AppUser user)
+    {
+        var confirm = await DisplayAlert("Sure!", $"Delete '{user.UserName}' Permanently?", "Yes", "No");
+        if (confirm)
+        {
+            /*
+             delete daily items
+             delete monthly plans
+             delete Field templates
+             delete users
+             */
+
+            var dailyRepo = _serviceProvider.GetRequiredService<IRepository<DailyEntry>>();
+            var planRepo = _serviceProvider.GetRequiredService<IRepository<MonthlyTarget>>();
+            var fieldRepo = _serviceProvider.GetRequiredService<IRepository<FieldTemplate>>();
+
+            var dailyReportsTask = dailyRepo.GetListAsync(d => d.UserId == user.Id);
+            var plansTask = planRepo.GetListAsync(d => d.UserId == user.Id);
+            var fieldsTask = fieldRepo.GetListAsync(d => d.UserId == user.Id);
+
+            await Task.WhenAll(dailyReportsTask, plansTask, fieldsTask);
+
+            var dailyReports = dailyReportsTask.Result;
+            var plans = plansTask.Result;
+            var fields = fieldsTask.Result;
+
+            foreach (var d in dailyReports)
+            {
+                await dailyRepo.DeleteAsync(d);
+            }
+
+            foreach (var p in plans)
+            {
+                await planRepo.DeleteAsync(p);
+            }
+
+            foreach (var f in fields)
+            {
+                await fieldRepo.DeleteAsync(f);
+            }
+
+            await _repository.DeleteAsync(user);
+        }
+    }
 }
