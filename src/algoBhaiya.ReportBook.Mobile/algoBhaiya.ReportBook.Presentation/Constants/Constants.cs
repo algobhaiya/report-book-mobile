e64991d2d6cb5c559e1264f632a7e298aaaf1a1b
algoBhaiya <algobhaiya@gmail.com>
2025-05-14 06:45:27 +0600

    Constants type moving static to const

--- src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.Presentation/Constants/Constants.cs
+++ src/algoBhaiya.ReportBook.Mobile/algoBhaiya.ReportBook.Presentation/Constants/Constants.cs
@@ -4,42 +4,42 @@
     {
         public static class FieldUnit
         {
-            public static string Item_ToEdit = "FieldUnit_Item_ToEdit";
-            public static string Action_OnUnitSaved = "FieldUnit_Action_OnUnitSaved";
+            public const string Item_ToEdit = "FieldUnit_Item_ToEdit";
+            public const string Action_OnUnitSaved = "FieldUnit_Action_OnUnitSaved";
         }
 
         public static class UnitType
         {
-            public static string Double = "double";
-            public static string Bool = "bool";
+            public const string Double = "double";
+            public const string Bool = "bool";
         }
 
         public static class FieldTemplate
         {
-            public static string Item_ToEdit = "FieldTemplate_Item_ToEdit";
-            public static string Action_OnUnitSaved = "FieldTemplate_Action_OnUnitSaved";
+            public const string Item_ToEdit = "FieldTemplate_Item_ToEdit";
+            public const string Action_OnUnitSaved = "FieldTemplate_Action_OnUnitSaved";
         }
 
         public static class DailyEntry
         {
-            public static string Item_SelectedDate = "DailyEntry_Item_SelectedDate";
+            public const string Item_SelectedDate = "DailyEntry_Item_SelectedDate";
         }
 
         public static class AppUser
         {
-            public static string CurrentUserId = "CurrentUserId";
+            public const string CurrentUserId = "CurrentUserId";
         }
 
         public static class Setting
         {
-            public static string ModificationDuration = "Setting_ModificationDuration";
-            public static string DataRemovalPeriod = "Setting_DataRemovalPeriod";
+            public const string ModificationDuration = "Setting_ModificationDuration";
+            public const string DataRemovalPeriod = "Setting_DataRemovalPeriod";
         }
 
         public static class AppState
         {
-            public static string LastCleanupDateKey = "last_cleanup_date";
-            public static string FirstRunKey = "IsSeedingInitialDataCompleted";
+            public const string LastCleanupDateKey = "last_cleanup_date";
+            public const string FirstRunKey = "IsSeedingInitialDataCompleted";
         }
 
         public static class LogIn
